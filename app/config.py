ceb147e08adc580a01809797cc1118937de899e5
syed fardeen <syedfardeen408@gmail.com>
2026-07-24 23:18:35 +0530

    RAG research assistant scaffold

---
+++ app/config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+    FLASK_SECRET_KEY = os.environ.get("FLASK_SECRET_KEY", "dev-key")
+    REDIS_URL = os.environ.get("REDIS_URL", "redis://localhost:6379/0")
+    FAISS_INDEX_PATH = os.environ.get("FAISS_INDEX_PATH", "indexes/faiss_core")
+    CHROMA_PERSIST_DIR = os.environ.get("CHROMA_PERSIST_DIR", "indexes/chroma_dynamic")
+    CHROMA_COLLECTION = os.environ.get("CHROMA_COLLECTION", "user_docs")
+    MAX_HISTORY_TURNS = int(os.environ.get("MAX_HISTORY_TURNS", 4))
